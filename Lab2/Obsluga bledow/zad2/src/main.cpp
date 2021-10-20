eb51c39114009564df136c5d2c6ecdd2494a65b9
ruffaaw <rafalciupek07@gmail.com>
2021-10-20 22:00:17 +0200

    add .

--- Lab2/Obsluga bledow/zad2/src/main.cpp
+++ Lab2/Obsluga bledow/zad2/src/main.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
 
+using namespace std;
+
+int konwertowanie(string ciag_znakow){
+    int przekonwertowany_ciag_znakow;
+    przekonwertowany_ciag_znakow = stoi(ciag_znakow);
+
+    return przekonwertowany_ciag_znakow;
+}
+int main(){
+    string ciag_znakow;
+    int przekonwertowany_ciag_znakow;
+
+    cout << "Podaj ciag znakow do przekonwertowania:"<<endl;
+    cin >> ciag_znakow;
+
+    try{
+        cout << konwertowanie(ciag_znakow) << endl;
+
+    }
+    catch(std::invalid_argument& e)
+    {
+        cout << "Wprowadzony ciag zawiera nieprawidlowe znaki" << endl;
+    }
+    catch(std::out_of_range& e)
+    {
+        cout << "Podana wartosc jest za duza" << endl;
+    }
+}
