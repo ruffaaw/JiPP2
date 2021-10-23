412966327865f47f989f87a1d0d9d5af9632889b
ruffaaw <rafalciupek07@gmail.com>
2021-10-23 13:05:37 +0200

    poczatek projektu

--- matrixApp/matrixLib/include/lib.h
+++ matrixApp/matrixLib/include/lib.h
@@ -1,4 +1,8 @@
 
-void wypisz(int **macierz, int ilosc_wierszy, int ilosc_kolumn);
+int rozmiar_macierzy(int rozmiar);
+int **utworz_macierz(int ilosc_wierszy, int ilosc_kolumn);
+void wypelnianie_macierzy(int **macierz, int ilosc_wierszy, int ilosc_kolumn);
+void wyswietl(int **macierz, int ilosc_wierszy, int ilosc_kolumn);
 int **addMatrix(int **macierz_a, int **macierz_b, int ilosc_wierszy, int ilosc_kolumn);
 int **subtractMatrix(int **macierz_a, int **macierz_b, int ilosc_wierszy, int ilosc_kolumn);
+int **multiplyMatrix(int **macierz_a, int **macierz_b, int ilosc_wierszy_a, int ilosc_kolumn_a, int ilosc_kolumn_b);
--- matrixApp/matrixLib/src/lib.cpp
+++ matrixApp/matrixLib/src/lib.cpp
@@ -4,7 +4,30 @@
 
 using namespace std;
 
-void wypisz(int **macierz, int ilosc_wierszy, int ilosc_kolumn){
+int rozmiar_macierzy(int rozmiar){
+    cin >> rozmiar;
+    return rozmiar;
+}
+
+int **utworz_macierz(int ilosc_wierszy, int ilosc_kolumn){
+    int **macierz;
+    macierz = new int * [ilosc_wierszy];
+
+    for (int i = 0; i < ilosc_wierszy; i++){
+        macierz[i] = new int [ilosc_kolumn];
+    }
+
+    return macierz;
+}
+void wypelnianie_macierzy(int **macierz, int ilosc_wierszy, int ilosc_kolumn){
+    for (int i = 0; i < ilosc_wierszy; i++){
+        for(int j = 0; j < ilosc_kolumn; j++){
+            cin >> macierz[i][j];
+        }
+    }
+}
+
+void wyswietl(int **macierz, int ilosc_wierszy, int ilosc_kolumn){
     for (int i = 0; i < ilosc_wierszy; i++){
         for(int j = 0; j < ilosc_kolumn; j++){
             cout << setw(5) << macierz[i][j];
@@ -15,10 +38,7 @@ void wypisz(int **macierz, int ilosc_wierszy, int ilosc_kolumn){
 
 
 int **addMatrix(int **macierz_a, int **macierz_b, int ilosc_wierszy, int ilosc_kolumn){
-    int **macierz_w = 0;
-    macierz_w = new int *[ilosc_wierszy];
-    for (int i = 0; i<ilosc_wierszy; i++)
-        macierz_w[i] = new int [ilosc_kolumn];
+    int **macierz_w = utworz_macierz(ilosc_wierszy, ilosc_kolumn);
 
     for (int i = 0; i < ilosc_wierszy; i++){
         for(int j = 0; j < ilosc_kolumn; j++){
@@ -28,17 +48,37 @@ int **addMatrix(int **macierz_a, int **macierz_b, int ilosc_wierszy, int ilosc_k
     return macierz_w;
 }
 
-int **subtractMatrix(int **macierz_a, int **macierz_b, int ilosc_wierszy, int ilosc_kolumn){
+int **subtractMatrix(int **macierz_a, int **macierz_b, int ilosc_wierszy, int ilosc_kolumn) {
+/*
     int **macierz_w = NULL;
     macierz_w = new int *[ilosc_wierszy];
     for (int i = 0; i<ilosc_wierszy; i++)
         macierz_w[i] = new int [ilosc_kolumn];
+*/
+    int **macierz_w = utworz_macierz(ilosc_wierszy, ilosc_kolumn);
 
-    for (int i = 0; i < ilosc_wierszy; i++){
-        for(int j = 0; j < ilosc_kolumn; j++){
+    for (int i = 0; i < ilosc_wierszy; i++) {
+        for (int j = 0; j < ilosc_kolumn; j++) {
             macierz_w[i][j] = macierz_a[i][j] - macierz_b[i][j];
         }
     }
     return macierz_w;
 }
+
+
+int **multiplyMatrix(int **macierz_a, int **macierz_b, int ilosc_wierszy_a, int ilosc_kolumn_a, int ilosc_kolumn_b){
+    int **macierz_w = utworz_macierz(ilosc_wierszy_a, ilosc_kolumn_b);
+
+    for (int i = 0; i < ilosc_wierszy_a; i++){
+        for(int j = 0; j < ilosc_kolumn_b; j++){
+            int suma_iloczynow = 0;
+            for (int k = 0; k < ilosc_kolumn_a; k++)
+                suma_iloczynow += (macierz_a[i][k] * macierz_b[k][j]);
+            macierz_w[i][j] = suma_iloczynow;
+        }
+    }
+    return macierz_w;
+}
+
+
 
