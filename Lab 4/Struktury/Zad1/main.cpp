a1c1ba99b48804ce1d629cc02db245c8fa257337
ruffaaw <rafalciupek07@gmail.com>
2021-10-30 14:46:44 +0200

    Zakonczenie zadania 1 z struktur

---
+++ Lab 4/Struktury/Zad1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+struct samochod{
+    string marka;
+    string model;
+    int rok_produkcji;
+    string kolor;
+};
+
+int main(){
+    int liczba_modeli = 4;
+    samochod model[] = {{"BMW", "M2", 2020, "czarny"},
+                          {"Ford", "Mustang", 2021, "szary"},
+                          {"Mazda", "6", 2016, "czerwony"},
+                          {"Tesla", "Model X", 2021, "zielony"}};
+
+    cout << "Marka\tModel\tRok produkcji\tKolor" << endl;
+    cout << "--------------------------------------" << endl;
+
+    for (int i = 0; i < liczba_modeli; i++){
+        cout << model[i].marka << "\t" << model[i].model << "\t" << model[i].rok_produkcji << "\t\t" << model[i].kolor << endl;
+    }
+    return 0;
+}
