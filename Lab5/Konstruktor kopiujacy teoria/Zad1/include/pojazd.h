41c78f7f2e4bbdf4a9eb8787c04427c98740c1dd
ruffaaw <rafalciupek07@gmail.com>
2021-11-09 11:46:15 +0100

    zaczęte zadanie pierwsze z konstruktorow kopiujacych

---
+++ Lab5/Konstruktor kopiujacy teoria/Zad1/include/pojazd.h
@@ -0,0 +1,19 @@
+#ifndef E1NOCOPYINGCONSTRUCTOR_POJAZD_H
+#define E1NOCOPYINGCONSTRUCTOR_POJAZD_H
+
+#include <iostream>
+using namespace std;
+
+class Pojazd{
+private:
+    int nr_rejestracyjny;
+    string nazwa;
+    int ilosc_miejsc;
+    string *dane_pasazera;
+    string marka_typ;
+
+public:
+
+
+};
+#endif //E1NOCOPYINGCONSTRUCTOR_POJAZD_H
