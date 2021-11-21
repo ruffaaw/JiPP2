df49c173b3a0f22b8536158b73ab6b4ebd9bf398
ruffaaw <rafalciupek07@gmail.com>
2021-11-21 19:44:32 +0100

    zakonczone zadanie z vectorami

--- Lab6/Vector/src/main.cpp
+++ Lab6/Vector/src/main.cpp
@@ -4,18 +4,6 @@
 using namespace std;
 
 int main(){
-    //Vector v1(1,2);
-    Vector v1;
+    Vector v1(1,2);
     cout << v1 << endl;
-    Vector v2(5,6);
-    Imaginary l(2, 5);
-    Imaginary z(3,-7);
-    //double i;
-    //v1.print();
-    //v2.print();
-
-    //v1.print()
-
-    l.print_imaginary();
-
 }
